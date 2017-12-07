10ee1bca601fcf5094a31639165babb6949bf2eb
unknown <jeronimo_y_g@hotmail.com>
2017-12-07 01:44:15 -0500

    inicio pantalla de compras

--- pre-js/interaction.js
+++ pre-js/interaction.js
@@ -5,4 +5,14 @@ $(document).ready(function($) {
         $('.pull-right').delay(60).slideToggle(240);
     });
 
+    $('.side-library h3').on('click', function () {
+        $(this).siblings('.main-library').slideToggle(500);
+        $(this).siblings('.search-icon').slideToggle(200);
+        $(this).siblings('.filter').slideToggle(200);
+    });
+
+    $("a[href='#']").on('click', function (e) {
+        e.preventDefault();
+    });
+
 });
